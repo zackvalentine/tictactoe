1c4959a335cd5bd5030bcb834af9cf552c1db8a6
Zack Valentine <zack.valentine22@gmail.com>
2021-07-24 11:16:36 -0500

    show winner & allow player 1 to win horizontally at top

--- tictactoe.js
+++ tictactoe.js
@@ -1,4 +1,6 @@
 let turnCounter = 0;
+let player1 = [];
+let player2 = [];
 
 const add = thisBoxId => {
     const board = document.getElementById("board");
@@ -9,8 +11,10 @@ const add = thisBoxId => {
         turnCounter++;
         if(turnCounter % 2 == 1) {
             newBox.innerHTML = "X";
+            player1.push(thisBoxId);
         } else {
             newBox.innerHTML = "O";
+            player2.push(thisBoxId);
         }
     }
     board.appendChild(newBox);
@@ -21,5 +25,27 @@ const setUpBoard = () => {
         add(i);
     }
 };
+
+const showWinner = () => {
+    const board = document.getElementById('board');
+    let isPlayer1Winner = checkForWinner(player1);
+    let isPlayer2Winner = false;
+    if(isPlayer1Winner) {
+        board.innerHTML = "Player 1 Wins!"
+    } else if(isPlayer2Winner) {
+        board.innerHTML = "Player 2 Wins!"
+    } else {
+        board.innerHTML = "No Winner"
+    }
+}
+
+const checkForWinner = player => {
+    if(player.includes(1)
+    && player.includes(2)
+    && player.includes(3)) {
+        return true;
+    }
+    return false;
+}
 
 document.addEventListener("DOMContentLoaded", setUpBoard);

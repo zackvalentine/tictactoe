49f071e8f15451208f9d0b1f4ba2fcf4804b91aa
Zack Valentine <zack.valentine22@gmail.com>
2021-07-24 11:02:50 -0500

    set up board on load & use arrow functions

--- tictactoe.js
+++ tictactoe.js
@@ -1,14 +1,16 @@
-function add(thisBoxId) {
+const add = thisBoxId => {
     const board = document.getElementById("board");
     let newBox = document.createElement('div');
     newBox.className = 'box';
     newBox.id = thisBoxId;
     newBox.innerHTML = thisBoxId;
     board.appendChild(newBox);
-}
+};
 
-function setUpBoard() {
+const setUpBoard = () => {
     for(let i = 1; i <= 9; i++) {
         add(i);
     }
-}
+};
+
+document.addEventListener("DOMContentLoaded", setUpBoard);
